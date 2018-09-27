27458be6c78815c23702acd15235705d4dd36fc5
Sheldon <sheldon@sheldonfweb.com>
2018-09-27 14:26:01 -0400

    comment description added to functions + changed sortCandidates name

--- js/App.js
+++ js/App.js
@@ -11,12 +11,14 @@ const candidates2012 = [];
 //   .then(blob => blob.json())
 //   .then(data => candidates2012.push(...data));
 
-function sortCandidates(api, array) {
+// fetch and push data from api
+function getCandidates(api, array) {
   fetch(api)
     .then(blob => blob.json())
     .then(data => array.push(...data.results));
 }
 
+// Sort and displays the Candidate Info
 function showCandidates(array) {
   array.sort((a, b) => a.name - b.name);
   array.forEach((elem) => {
@@ -24,8 +26,8 @@ function showCandidates(array) {
   });
 }
 
-sortCandidates(twelveOne, candidates2012);
-sortCandidates(twelveTwo, candidates2012);
+getCandidates(twelveOne, candidates2012);
+getCandidates(twelveTwo, candidates2012);
 // for use on event listener or onload event
 showCandidates(candidates2012);
 
