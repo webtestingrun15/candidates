12e6e5768e7a67a8a0f09497bf1dfa8adcc5412d
Sheldon <sheldon@sheldonfweb.com>
2018-10-02 11:37:08 -0400

    grabbed and displayed 2016 data in the console

--- js/App.js
+++ js/App.js
@@ -4,9 +4,15 @@ const twelveOne =
 const twelveTwo =
   'https://api.open.fec.gov/v1/candidates/?has_raised_funds=true&election_year=2012&office=P&page=2&sort=name&per_page=100&api_key=5b4BDawDYA6q0JIqASIr6vlas4xEQvZCQlu78SQj';
 
+const sixteenOne =
+  'https://api.open.fec.gov/v1/candidates/?has_raised_funds=true&election_year=2016&office=P&page=1&sort=name&per_page=100&api_key=5b4BDawDYA6q0JIqASIr6vlas4xEQvZCQlu78SQj';
+
+const sixteenTwo =
+  'https://api.open.fec.gov/v1/candidates/?has_raised_funds=true&election_year=2016&office=P&page=2&sort=name&per_page=100&api_key=5b4BDawDYA6q0JIqASIr6vlas4xEQvZCQlu78SQj';
+
 
 const candidates2012 = [];
-
+const candidates2016 = [];
 // fetch(twelveOne)
 //   .then(blob => blob.json())
 //   .then(data => candidates2012.push(...data));
@@ -28,8 +34,11 @@ function showCandidates(array) {
 
 getCandidates(twelveOne, candidates2012);
 getCandidates(twelveTwo, candidates2012);
+getCandidates(sixteenOne, candidates2016);
+getCandidates(sixteenTwo, candidates2016);
 // for use on event listener or onload event
-showCandidates(candidates2012);
+// showCandidates(candidates2012);
+// showCandidates(candidates2016);
 
 /* TODO: Loop over 2012 with a .foreach or .map and maybe using a filter each  using candidates2012["#"].results
 
